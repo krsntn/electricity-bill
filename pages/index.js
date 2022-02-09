d03d8c74f483b778d72ae18ee91b77415aac3c5c
krsntn <kanahan0110@gmail.com>
2022-02-09 21:24:56 +0800

    revamp interface

--- pages/index.js
+++ pages/index.js
@@ -78,29 +78,28 @@ export default function Home() {
   }, [mode]);
 
   return (
-    <div>
+    <div className="bg-green-500 min-h-screen text-white">
       <Head>
         <title>Electricity Bill Calculator</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="text-white min-h-full">
-        <div className="bg-green-500  divide-y-2 divide-solid">
-          <section className="mx-auto py-16 text-3xl w-10/12 max-w-4xl">
-            <h1 className="font-semibold text-4xl md:text-6xl pb-2 mb-4">
+      <main className="flex flex-col items-center justify-between xl:flex-row m-auto max-w-screen-xl">
+          <section className="mx-auto py-6 w-9/12 xl:w-7/12 xl:mx-0 xl:px-6">
+            <h1 className="font-semibold text-xl py-6">
               FORECAST YOUR ELECTRICITY BILL
             </h1>
 
-            <p className="mb-4 py-2 px-4 text-base bg-red-400 rounded-xl">
+            <p className="mb-4 py-2 px-4 bg-red-400 rounded-xl">
               Disclaimer: This calculation is mainly for Residential Customer's
               reference. Please note that this only serves as a general estimate
               and consumers should not rely on this as a final figure.
             </p>
 
-            <div className="flex flex-col items-stretch gap-2 md:flex-row justify-between md:items-center mb-4">
+            <div className="flex flex-col items-stretch md:flex-row justify-between md:items-center">
               <button
                 type="button"
-                className={`flex-1 ${
+                className={`md:rounded-tl-lg flex-1 ${
                   mode === MODE.kWh ? "bg-green-700" : "bg-green-600"
                 } py-4 font-semibold hover:bg-green-700 focus:bg-green-700`}
                 onClick={() => setMode(MODE.kWh)}
@@ -109,7 +108,7 @@ export default function Home() {
               </button>
               <button
                 type="button"
-                className={`flex-1 ${
+                className={`md:rounded-tr-lg flex-1 ${
                   mode === MODE.W ? "bg-green-700" : "bg-green-600"
                 } py-4 font-semibold hover:bg-green-700 focus:bg-green-700`}
                 onClick={() => setMode(MODE.W)}
@@ -118,14 +117,14 @@ export default function Home() {
               </button>
             </div>
 
-            <form className="grid grid-cols-3" onSubmit={calc}>
+            <form className="pt-4 md:pt-0 grid grid-cols-3" onSubmit={calc}>
               {mode === MODE.kWh && (
-                <div className="col-span-full bg-white text-black py-6 px-8 flex flex-col items-start md:flex-row justify-between md:items-center mb-4">
+                <div className="col-span-full bg-green-700 py-4 px-8 flex flex-col items-start md:flex-row justify-between md:items-center mb-4">
                   <div className="pb-4 md:pb-0">Total Consumption (kWh)</div>
                   <input
                     type="number"
                     step={0.0001}
-                    className="rounded-lg border border-gray-700 p-2 focus:outline-none focus:ring-2 focus:border-blue-500 w-full md:w-auto"
+                    className="bg-green-800 p-2 focus:outline-none focus:ring-2 focus:border-blue-500 w-full md:w-auto"
                     value={inputKWH}
                     onChange={(e) => setInputKWH(e.target.value)}
                   />
@@ -134,32 +133,32 @@ export default function Home() {
 
               {mode === MODE.W && (
                 <React.Fragment>
-                  <div className="col-span-full bg-white text-black py-6 px-8 flex flex-col items-start md:flex-row justify-between md:items-center mb-4">
+                  <div className="col-span-full bg-green-700 py-4 px-8 flex flex-col items-start md:flex-row justify-between md:items-center">
                     <div className="pb-4 md:pb-0">Power Comsumption (W)</div>
                     <input
                       type="number"
                       step={0.0001}
-                      className="rounded-lg border border-gray-700 p-2 focus:outline-none focus:ring-2 focus:border-blue-500 w-full md:w-auto"
+                      className="bg-green-800 p-2 focus:outline-none focus:ring-2 focus:border-blue-500 w-full md:w-auto"
                       value={inputW}
                       onChange={(e) => setInputW(e.target.value)}
                     />
                   </div>
-                  <div className="col-span-full bg-white text-black py-6 px-8 flex flex-col items-start md:flex-row justify-between md:items-center mb-4">
+                  <div className="col-span-full bg-green-700 py-4 px-8 flex flex-col items-start md:flex-row justify-between md:items-center">
                     <div className="pb-4 md:pb-0">Total Hours Per Day</div>
                     <input
                       type="number"
                       max="24"
-                      className="rounded-lg border border-gray-700 p-2 focus:outline-none focus:ring-2 focus:border-blue-500 w-full md:w-auto"
+                      className="bg-green-800 p-2 focus:outline-none focus:ring-2 focus:border-blue-500 w-full md:w-auto"
                       value={inputHours}
                       onChange={(e) => setInputHours(e.target.value)}
                     />
                   </div>
-                  <div className="col-span-full bg-white text-black py-6 px-8 flex flex-col items-start md:flex-row justify-between md:items-center mb-4">
+                  <div className="col-span-full bg-green-700 py-4 px-8 flex flex-col items-start md:flex-row justify-between md:items-center mb-4">
                     <div className="pb-4 md:pb-0">Total Days Per Month</div>
                     <input
                       type="number"
                       max="31"
-                      className="rounded-lg border border-gray-700 p-2 focus:outline-none focus:ring-2 focus:border-blue-500 w-full md:w-auto"
+                      className="bg-green-800 p-2 focus:outline-none focus:ring-2 focus:border-blue-500 w-full md:w-auto"
                       value={inputDays}
                       onChange={(e) => setInputDays(e.target.value)}
                     />
@@ -176,114 +175,74 @@ export default function Home() {
             </form>
           </section>
 
-          {bill !== null && (
-            <section className="mx-auto grid grid-cols-3 py-16 text-3xl w-10/12 max-w-4xl">
+          <div className="mx-auto py-6 w-9/12 xl:mr-0 xl:w-5/12 xl:px-6">
+            <section className="mx-auto xl:mr-0 grid grid-cols-3">
               <h1
                 id="estimatedBill"
-                className="col-span-full font-semibold text-2xl pb-4"
+                className="col-span-full font-semibold text-xl pb-4 text-center"
               >
-                YOUR ESTIMATED BILL
+                Estimated Bill
               </h1>
 
-              <div className="col-span-full text-2xl flex flex-col items-stretch justify-between gap-2 md:flex-row md:items-stretch mb-2">
-                <div className="flex-1 bg-white text-black p-8">
-                  <div className="mb-4">Bill</div>
-
-                  <div className="font-bold">
-                    <em className="text-base align-top mr-2">RM</em>
-                    {bill.reduce((amt, cur) => amt.add(cur)).toFixed(2)}
-                  </div>
+              <div className="col-span-full bg-blue-100 text-black py-6 px-8 mb-4 flex flex-col items-start md:flex-row justify-between md:items-center">
+                <div className="text-lg mb-4 md:mb-0 font-semibold">
+                  Total kWh
                 </div>
-
-                <div className="flex-1 bg-white text-black p-8">
-                  <div className="mb-4">Service Tax</div>
-                  <div className="font-bold">
-                    <em className="text-base align-top mr-2">RM</em>
-                    {bill
-                      .reduce((amt, cur, index) => {
-                        if (index >= 3) {
-                          return amt.add(cur.times(0.06));
-                        }
-                        return new Decimal(0);
-                      })
-                      .toFixed(2)}
-                  </div>
+                <div className="self-end md:self-auto">
+                {mode === MODE.kWh && inputKWH ? inputKWH : mode === MODE.W && inputW ? new Decimal(inputW).dividedBy(1000).times(inputHours).times(inputDays).toString():  "0"}
                 </div>
               </div>
 
-              <div className="col-span-full bg-red-500 text-white py-6 px-8 mb-4 flex flex-col items-start md:flex-row justify-between md:items-center">
-                <div className="text-2xl mb-4 md:mb-0">
-                  Total bill inclusive of Service Tax is
-                </div>
-                <div className="text-3xl font-bold self-end md:self-auto">
-                  <em className="text-base align-top mr-2">RM</em>
-                  {bill
-                    .reduce((amt, cur, index) => {
-                      if (index >= 3) {
-                        cur = cur.times(1.06);
-                      }
-                      return amt.add(cur);
-                    })
-                    .toFixed(2)}
-                </div>
-              </div>
-            </section>
-          )}
-
-          {bill !== null && (
-            <section className="mx-auto grid grid-cols-3 py-16 text-3xl w-10/12 max-w-4xl">
-              <h1
-                id="estimatedBill"
-                className="col-span-full font-semibold text-2xl pb-4"
-              >
-                CALCULATION DETAILS
-              </h1>
-
-              <div className="col-span-full text-2xl flex flex-col items-stretch justify-between gap-2 md:flex-row md:items-stretch mb-4">
-                <table className="table-auto w-full bg-green-800 text-center border-collapse">
+              <div className="text-black col-span-full flex flex-col items-stretch justify-between gap-2 md:flex-row md:items-stretch mb-2">
+                <table className="table-auto w-full bg-blue-200 text-center border-collapse">
                   <thead>
-                    <tr className="text-lg">
-                      <th className="p-2">Total kWh</th>
-                      <th className="p-2">
+                    <tr className="text-md">
+                      <th className="border-b border-green-600 py-4">kWh</th>
+                      <th className="border-b border-green-600 py-4">Rate</th>
+                      <th className="border-b border-green-600 py-4">
                         Cost +{" "}
-                        <span className="text-purple-400">Service Tax</span>
+                        <span className="text-blue-400">Service Tax</span>
                       </th>
                     </tr>
                   </thead>
                   <tbody>
                     <tr>
-                      <td className="border border-green-600 p-2">1 - 200</td>
-                      <td className="border border-green-600 p-2 font-bold">
+                      <td className="bg-blue-100 p-2">1 - 200</td>
+                      <td className="bg-blue-100 p-2">{new Decimal(rates[0]).dividedBy(100).toString()}</td>
+                      <td className="bg-blue-100 p-2">
                         <em className="text-base align-top mr-2">RM</em>
-                        {bill[0].toFixed(2)}
+                        {bill ? bill[0].toFixed(2) : "0.00"}
                       </td>
                     </tr>
 
                     <tr>
-                      <td className="border border-green-600 p-2">201 - 300</td>
-                      <td className="border border-green-600 p-2 font-bold">
+                      <td className="p-2">201 - 300</td>
+                      <td className="p-2">{new Decimal(rates[1]).dividedBy(100).toString()}</td>
+                      <td className="p-2">
                         <em className="text-base align-top mr-2">RM</em>
-                        {bill[1].toFixed(2)}
+                        {bill ? bill[1].toFixed(2) : "0.00"}
                       </td>
                     </tr>
 
                     <tr>
-                      <td className="border border-green-600 p-2">301 - 600</td>
-                      <td className="border border-green-600 p-2 font-bold">
+                      <td className="bg-blue-100 p-2">301 - 600</td>
+                      <td className="bg-blue-100 p-2">{new Decimal(rates[2]).dividedBy(100).toString()}</td>
+                      <td className="bg-blue-100 p-2">
                         <em className="text-base align-top mr-2">RM</em>
-                        {bill[2].toFixed(2)}
+                        {bill ? bill[2].toFixed(2) : "0.00"}
                       </td>
                     </tr>
 
                     <tr>
-                      <td className="border border-green-600 p-2">601 - 900</td>
-                      <td className="border border-green-600 p-2 font-bold">
+                      <td className="p-2">601 - 900</td>
+                      <td className="p-2">{new Decimal(rates[3]).dividedBy(100).toString()}</td>
+                      <td className="p-2">
                         <em className="text-base align-top mr-2">RM</em>
-                        {bill[3].toFixed(2)}{" "}
-                        {bill[3] > 0 ? (
+                        {bill ? bill[3].toFixed(2) : "0.00"}
+                        {bill && bill[3] > 0 ? (
                           <React.Fragment>
-                            +{" "}
-                            <span className="text-purple-400">
+                            {" "}+{" "}
+                            <span className="text-blue-400">
                               <em className="text-base align-top mr-2">RM</em>
                               {(bill[3] * 0.06).toFixed(2)}
                             </span>
@@ -295,14 +254,15 @@ export default function Home() {
                     </tr>
 
                     <tr>
-                      <td className="border border-green-600 p-2">> 900</td>
-                      <td className="border border-green-600 p-2 font-bold">
+                      <td className="bg-blue-100 p-2">{">"} 900</td>
+                      <td className="bg-blue-100 p-2">{new Decimal(rates[4]).dividedBy(100).toString()}</td>
+                      <td className="bg-blue-100 p-2">
                         <em className="text-base align-top mr-2">RM</em>
-                        {bill[4].toFixed(2)}{" "}
-                        {bill[4] > 0 ? (
+                        {bill ? bill[4].toFixed(2) : "0.00"}
+                        {bill && bill[4] > 0 ? (
                           <React.Fragment>
-                            +{" "}
-                            <span className="text-purple-400">
+                            {" "}+{" "}
+                            <span className="text-blue-400">
                               <em className="text-base align-top mr-2">RM</em>
                               {(bill[4] * 0.06).toFixed(2)}
                             </span>
@@ -315,24 +275,62 @@ export default function Home() {
                   </tbody>
                 </table>
               </div>
-            </section>
-          )}
-        </div>
-      </main>
 
-      <footer className="w-full h-16 border-t-2 border-fuchsia-500 flex justify-center items-center">
-        <div>
-          Crafted by{" "}
-          <a
-            className="text-blue-500"
-            href="https://dev.krsn.xyz"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            karson
-          </a>
-        </div>
-      </footer>
+              <div className="col-span-full flex flex-col items-stretch justify-between gap-2 md:flex-row md:items-stretch mb-2">
+                <div className="flex-1 bg-blue-200 text-black p-8 text-right">
+                  <div className="mb-4 text-lg font-semibold text-left">Bill</div>
+
+                  <div>
+                    <em className="text-base align-top mr-2">RM</em>
+                    {bill ? bill.reduce((amt, cur) => amt.add(cur)).toFixed(2): "0.00"}
+                  </div>
+                </div>
+
+                <div className="flex-1 bg-blue-200 text-black p-8 text-right">
+                  <div className="mb-4 text-lg font-semibold text-left">Service Tax</div>
+                  <div>
+                    <em className="text-base align-top mr-2">RM</em>
+                    {bill ? bill.reduce((amt, cur, index) => {
+                        if (index >= 3) {
+                          return amt.add(cur.times(0.06));
+                        }
+                        return new Decimal(0);
+                      })
+                      .toFixed(2): "0.00"}
+                  </div>
+                </div>
+              </div>
+
+              <div className="col-span-full bg-blue-100 text-black py-6 px-8 mb-4 flex flex-col items-start md:flex-row justify-between md:items-center">
+                <div className="text-lg mb-4 md:mb-0 font-semibold">
+                  Total Bill
+                </div>
+                <div className="self-end md:self-auto">
+                  <em className="text-base align-top mr-2">RM</em>
+                  {bill ? bill.reduce((amt, cur, index) => {
+                      if (index >= 3) {
+                        cur = cur.times(1.06);
+                      }
+                      return amt.add(cur);
+                    })
+                    .toFixed(2): "0.00"}
+                </div>
+              </div>
+
+              <div className="col-span-full text-right text-xs text-gray-700">
+                Crafted by{" "}
+                <a
+                  className="text-blue-800"
+                  href="https://dev.krsn.xyz"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  karson
+                </a>
+              </div>
+            </section>
+          </div>
+      </main>
 
       <style jsx global>{`
         html,
